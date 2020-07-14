4f359b3f816197de318dafcf910f880f5941947e
nmegrant <nme.grant@gmail.com>
2020-07-14 20:27:34 +0200

    Move title to separate component

--- src/App.js
+++ src/App.js
@@ -1,20 +1,11 @@
 import React from "react";
 import "./App.css";
-import Jumbotron from "react-bootstrap/Jumbotron";
-import Container from "react-bootstrap/Container";
+import MyTitleBar from "./Components/MyTitleBar";
 
 function App() {
   return (
     <div className="App">
-      <Jumbotron
-        fluid
-        style={{ backgroundColor: "rgba(102, 16, 242, 0.2)", padding: "20px" }}
-      >
-        <Container>
-          <h1>RNGn</h1>
-          <p>Pick the dice you want to roll.</p>
-        </Container>
-      </Jumbotron>
+      <MyTitleBar />
     </div>
   );
 }
---
+++ src/Components/MyTitleBar.js
@@ -0,0 +1,17 @@
+import React from "react";
+import Jumbotron from "react-bootstrap/Jumbotron";
+import Container from "react-bootstrap/Container";
+
+export default function MyTitleBar() {
+  return (
+    <Jumbotron
+      fluid
+      style={{ backgroundColor: "rgba(102, 16, 242, 0.2)", padding: "20px" }}
+    >
+      <Container>
+        <h1>RNG</h1>
+        <p>Pick the dice you want to roll.</p>
+      </Container>
+    </Jumbotron>
+  );
+}
