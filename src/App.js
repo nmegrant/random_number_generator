e7b8940dbf9e63a2619199ef0a3ad6691156ab13
nmegrant <nme.grant@gmail.com>
2020-07-14 21:14:16 +0200

    Set dice value

--- src/App.js
+++ src/App.js
@@ -3,13 +3,14 @@ import "./App.css";
 import MyTitleBar from "./Components/MyTitleBar";
 import Button from "react-bootstrap/Button";
 
-// import Switch from "react-switch";
+import Switch from "react-switch";
 
 function App() {
   const [roll, setRoll] = useState(0);
+  const [value, setValue] = useState(100);
 
-  function rollDice(dice) {
-    setRoll(Math.floor(Math.random() * 10));
+  function rollDice() {
+    setRoll(Math.floor(Math.random() * value));
   }
 
   return (
@@ -18,7 +19,27 @@ function App() {
       <Button variant="info" onClick={rollDice}>
         Roll
       </Button>
+
+      <h1>10</h1>
+
+      <Switch
+        checked={value}
+        onChange={() => {
+          if (value === 10) {
+            setValue(100);
+          } else {
+            setValue(10);
+          }
+        }}
+        checkedIcon={false}
+        uncheckedIcon={false}
+        onColor="#5bc0de"
+        offColor="#5bc0de"
+      />
+      <h1>100</h1>
+
       <h1>{roll}</h1>
+      <h1>{value}</h1>
     </div>
   );
 }
