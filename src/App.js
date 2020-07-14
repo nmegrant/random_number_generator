99e43f564da002f96c8e279a6dc91319181e2bf4
nmegrant <nme.grant@gmail.com>
2020-07-14 20:22:42 +0200

    Add Jumbotron title

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
 import React from "react";
-import logo from "./logo.svg";
 import "./App.css";
+import Jumbotron from "react-bootstrap/Jumbotron";
+import Container from "react-bootstrap/Container";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Jumbotron
+        fluid
+        style={{ backgroundColor: "rgba(102, 16, 242, 0.2)", padding: "20px" }}
+      >
+        <Container>
+          <h1>RNGn</h1>
+          <p>Pick the dice you want to roll.</p>
+        </Container>
+      </Jumbotron>
+    </div>
+  );
 }
 
 export default App;
