c93fe54145b846063ba6a3c8e3fbb9744199e5f0
nmegrant <nme.grant@gmail.com>
2020-07-14 21:51:37 +0200

    Set up RNG

--- src/App.js
+++ src/App.js
@@ -2,18 +2,22 @@ import React, { useState } from "react";
 import "./App.css";
 import MyTitleBar from "./Components/MyTitleBar";
 import Button from "react-bootstrap/Button";
-import Container from "react-bootstrap/Container";
-import Row from "react-bootstrap/Row";
 
 import Switch from "react-switch";
 
 function App() {
   const [roll, setRoll] = useState(0);
-  const [value, setValue] = useState(100);
+  // const [ten, setTen] = useState(false);
+
+  // const value = ten ? 10 : 100;
 
   function rollDice() {
-    setRoll(Math.floor(Math.random() * value));
+    setRoll(Math.floor(Math.random() * 10));
   }
+
+  // function handleChange(checked) {
+  //   setTen(!checked);
+  // }
 
   return (
     <div className="App">
@@ -21,25 +25,17 @@ function App() {
       <Button variant="info" onClick={rollDice}>
         Roll
       </Button>
-      <h1>10</h1>
+      {/* <h1>100</h1>
       <Switch
-        checked={value}
-        onChange={() => {
-          if (value === 10) {
-            setValue(100);
-          } else {
-            setValue(10);
-          }
-        }}
+        checked={false}
+        onChange={handleChange}
         checkedIcon={false}
         uncheckedIcon={false}
         onColor="#5bc0de"
         offColor="#5bc0de"
       />
-      <h1>100</h1>
-
+      <h1>10</h1> */}
       <h1>{roll}</h1>
-      <h1>{value}</h1>
     </div>
   );
 }
