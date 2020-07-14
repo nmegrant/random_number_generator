45e1dc756d22d2eedf3e2460e6ee309469b6485c
nmegrant <nme.grant@gmail.com>
2020-07-14 22:32:09 +0200

    Add set history

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
 import React, { useState } from "react";
 import "./App.css";
 import MyTitleBar from "./Components/MyTitleBar";
+
 import Button from "react-bootstrap/Button";
+import Container from "react-bootstrap/Container";
 
 import Switch from "react-switch";
 
 function App() {
   const [roll, setRoll] = useState(0);
   const [ten, setTen] = useState(false);
+  const [rollHistory, setRollHistory] = useState([]);
+
+  console.log(rollHistory);
 
   const value = ten ? 10 : 100;
 
@@ -21,17 +26,24 @@ function App() {
       <Button variant="info" onClick={rollDice}>
         Roll
       </Button>
-      <h1>100</h1>
-      <Switch
-        checked={ten}
-        onChange={() => setTen(!ten)}
-        checkedIcon={false}
-        uncheckedIcon={false}
-        onColor="#5bc0de"
-        offColor="#5bc0de"
-      />
-      <h1>10</h1>
+      <Container style={{ marginTop: "20px" }}>
+        <span>
+          100
+          <Switch
+            checked={ten}
+            onChange={() => setTen(!ten)}
+            checkedIcon={false}
+            uncheckedIcon={false}
+            onColor="#5bc0de"
+            offColor="#5bc0de"
+          />
+          10
+        </span>
+      </Container>
       <h1>{roll}</h1>
+      {rollHistory.map((roll) => (
+        <p>{roll}</p>
+      ))}
     </div>
   );
 }
